f0a88f73d1affb311433ebec2348047b70ba92d4
marvinjoel <marvin.roses@gmail.com>
2023-02-08 10:24:55 -0500

    feat: first views

--- Rawa_back/apps/accounts/views.py
+++ Rawa_back/apps/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class ListAll(APIView):
+
+    def get(self, request):
+        """
+        Return a list of all users.
+        """
+        return Response({"access": "seccess"})
--- Rawa_back/config/settings.py
+++ Rawa_back/config/settings.py
@@ -37,7 +37,7 @@ LOCAL_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
-
+    'apps.accounts',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
